bb2bbaefd1bf546f8e8b0df5bb746e86620a6360
= <=>
2024-06-23 15:37:51 +0100

    basic opentelementry set up

---
+++ src/main/java/com/chucheka/opentelemetry/OpentelemetryApplication.java
@@ -0,0 +1,13 @@
+package com.chucheka.opentelemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentelemetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpentelemetryApplication.class, args);
+	}
+
+}
